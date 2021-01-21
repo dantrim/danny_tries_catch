0b759e6c893022c72339396a561bef40eaffd729
dantrim <dantrim1023@gmail.com>
2021-01-21 15:56:49 -0800

    check nofail

--- tests/test_foo.h
+++ tests/test_foo.h
@@ -5,6 +5,6 @@
 
 TEST_CASE("Test of libfoo", "[foo]") {
     REQUIRE(foo::add1(0) == 1);
-    CHECK(foo::add1(4) == 4);
+    CHECK_NOFAIL(foo::add1(4) == 4);
     REQUIRE_FALSE(foo::add1(4) == 3);
 }
